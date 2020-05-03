0b1cbda9ba7b0c2a79c4fb4e61abe0d05f82cca8
Sowmiya Nagarajan <sowmiya_n@intuit.com>
2020-05-03 14:08:36 +0530

    Revert "Revert "Adding src""
    This reverts commit d2a64cad9b8885018c6aa6ba9e5067b73d199266.

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Download app stacks loaded with all your favourite clients and services along with code boilerplates.</p>
+    <v-btn x-large color="success" dark>Get Started</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+p {
+  margin: 10px 0 0;
+  font-family: 'Lato', sans-serif;
+  font-size: 1.2em;
+}
+h1 {
+  font-size: 4em;
+  letter-spacing: 10px;
+  margin: 0px;
+}
+</style>
