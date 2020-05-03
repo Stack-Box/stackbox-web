ebdec1036ae4a45574148e88736ce683b69c8d8f
Sowmiya Nagarajan <sowmiya_n@intuit.com>
2020-05-03 14:05:28 +0530

    Adding src

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Logo" width="25%" height="25%" src="./assets/logo.png">
+    <Home msg="STACKBOX"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
